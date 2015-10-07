d9cd7456ef9e3d1161fd1d3e3f62115dadca3111
Ariel Churi <git@sparklelabs.com>
2015-10-07 10:03:20 -0400

    added comments

--- led6fade03/sense.ino
+++ led6fade03/sense.ino
@@ -1,4 +1,4 @@
-int touchSense(){
+int touchSense(){ // a function to check the touch sensor and return the result
   long start = millis();
   long result =  cs_4_2.capacitiveSensor(30);
   return result;
